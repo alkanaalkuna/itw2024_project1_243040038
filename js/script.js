9292ad7f044df5483b5a232c4d8408988d5f2116
alkanaalkuna <regantraalkxen@gmail.com>
2025-01-06 13:36:22 +0700

    progress github part sekian

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+// Ambil elemen checkbox dan ul
+// const menuCheckbox = document.querySelector('.menu-toggle input');
+// const navMenu = document.querySelector('nav ul');
+
+// Tambahkan event listener untuk toggle
+// menuCheckbox.addEventListener('change', () => {
+//     if (menuCheckbox.checked) {
+//         navMenu.classList.add('slide');
+//     } else {
+//         navMenu.classList.remove('slide');
+//     }
+// });
+
+/** creating button click show hide navbar **/
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener("change", function() {
-    nav.classList.toggle("slide", menuToggle.checked);
+menuToggle.addEventListener("click", function() {
+    nav.classList.toggle("slide");
 });
