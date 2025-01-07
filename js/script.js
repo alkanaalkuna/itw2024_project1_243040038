cb9c3e791a4e20175a58959f42c155bd2eb903de
alkanaalkuna <regantraalkxen@gmail.com>
2025-01-07 10:56:22 +0700

    finall??

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// Ambil elemen checkbox dan ul
-// const menuCheckbox = document.querySelector('.menu-toggle input');
-// const navMenu = document.querySelector('nav ul');
-
-// Tambahkan event listener untuk toggle
-// menuCheckbox.addEventListener('change', () => {
-//     if (menuCheckbox.checked) {
-//         navMenu.classList.add('slide');
-//     } else {
-//         navMenu.classList.remove('slide');
-//     }
-// });
-
-/** creating button click show hide navbar **/
 const menuToggle = document.querySelector(".menu-toggler input");
 const nav = document.querySelector("nav ul");
 
